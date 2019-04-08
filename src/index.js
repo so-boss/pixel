6c0a25f69f1a17af65b65b02161cbbb147117244
Michael Kelly <email@michaelevan.com>
2019-04-08 14:58:40 -0700

    Started special type <Label> buildout and added outdated but simple and small accounting.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-// import React from 'react';
-// import { render } from "react-dom";
-// //import { TextInput } from "./lib";
-// import { Title, Thing, Wrapper, Label, Message } from "./lib";
-//
-//
-// const App = () => (
-//   <div style={{ width: 640, margin: "15px auto" }}>
-//       <Title>Hello Pixel</Title>
-//       <Thing>Hello Pixel</Thing>
-//       <Wrapper id='LabelAndMessage'>
-//           <Label
-//             random1='xyz'
-//           >Hello Pixel</Label>
-//           <Message>Hello Pixel</Message>
-//       </Wrapper>
-//   </div>
-// );
-//
-// render(<App />, document.getElementById("root"));
+import React from 'react';
+import { render } from "react-dom";
+
+import {Wrapper, Label } from "./lib";
+
+
+const App = () => (
+  <div style={{ width: 640, margin: "15px auto" }}>
+    <Wrapper>
+      <Label currency="$">1000</Label>
+      <Label currency="$" multi={2}>
+        {["$1M", "$2M"]}
+      </Label>
+    </Wrapper>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
--- src/lib/components/wrappers/Label/Label.js
+++ src/lib/components/wrappers/Label/Label.js
@@ -3,39 +3,70 @@ import { Thing } from './../../index.js';
 import React from "react";
 import PropTypes from 'prop-types';
 
-export default function Label ({ children, ...rest }) {
-    // render () {
-    //   const {
-    //     children,
-    //     ...rest
-    //   } = this.props;
+const accounting = require('accounting');
 
+function CurrencyValue({quantity, joinWith, children}) {
+  let processedValue;
+
+  // IF more than one value
+  // i.e. $25K / $50K
+  if(quantity && quantity>1) {
+    processedValue = children.join(joinWith);
+  } else {
+    // OTHERWISE only one value
+    processedValue = accounting.formatMoney(children, "$", 0);
+  }
+
+  return processedValue;
+}
+CurrencyValue.defaultProps = {
+  joinWith:" / ",
+  multi:1
+};
+CurrencyValue.propTypes = {
+  quantity: PropTypes.integer,
+  joinWith: PropTypes.string,
+  children: PropTypes.any
+};
+
+/*
+  <Label currency="$">1000</Label>
+  <Label currency="$" multi={2}>
+    ["$1M", "$2M"]
+   </Label>
+*/
+export default function Label ({ currency, multi, children, ...rest }) {
     return (
-      <Thing type="label" {...rest}>
-          {children}
+      <Thing
+        type="label"
+        currency={currency}
+        {...rest}
+      >
+        {
+          !currency
+          ? children
+          :(
+            <CurrencyValue
+              quantity={multi}
+            >
+              {children}
+            </CurrencyValue>
+          )
+        }
       </Thing>
     );
-    //}
 }
 
-// const Label = props => React.createElement(
-//  Thing,
-//   {
-//     id: props.id,
-//     type: props.type,
-//     pixel: props.pixel
-//   },
-//   props.children
-// );
-//
-// Label.defaultProps = {
-//   type: 'label',
-//   pixel: 'label'
-// };
+Label.defaultProps = {
+  currency:false,
+  multi:1
+}
 
 Label.propTypes = {
   id: PropTypes.string,
   type: PropTypes.string,
+  currency:PropTypes.any,
+  multi:PropTypes.number,
   pixel: PropTypes.string,
   children: PropTypes.any
 };
