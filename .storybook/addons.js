b7c7ba7f91d3afc9a7e63e4ce91d33e154ac5949
Michael Kelly <email@michaelevan.com>
2019-10-14 06:22:47 -0700

    Still stuck on the story bbook issue

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -5,9 +5,9 @@ import '@storybook/addon-knobs/register';
 // register the notes addon as a tab
 // import '@storybook/addon-notes/register';
 // or register the notes addon as a panel. Only one can be used!
-import '@storybook/addon-notes/register-panel';
+//import '@storybook/addon-notes/register-panel';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-viewport/register';
-import '@storybook/addon-design-assets/register';
+//import '@storybook/addon-design-assets/register';
 
 
